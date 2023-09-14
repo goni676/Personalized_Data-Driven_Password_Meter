469a985fd84136c0f65e9c130ec0c51838985406
goni676 <80840650+goni676@users.noreply.github.com>
2023-09-14 10:20:54 +0300

    Update create_CDF.py

--- create_CDF.py
+++ create_CDF.py
@@ -17,11 +17,11 @@ def plot(file, country, title, top_values):
             if line:
                 line = line.strip()
                 if title != "particular":
-                    tmp = PESrank.main(line, os.getcwd())
-                    data.append(tmp)
+                    rank, exp = PESrank.main(line, os.getcwd())
+                    data.append(rank)
                 else:
-                    tmp = PESrank_new.main(line, os.getcwd(), country, top_values)
-                    data.append(tmp)
+                    rank, exp, ind = PESrank_new.main(line, os.getcwd(), top_values, country=country)
+                    data.append(rank)
     data.sort()
     return data
 
@@ -39,16 +39,21 @@ def create_CDF(test_file, country):
     dict_result = {}
 
     # Define a list of 'n' values for experimentation.
-    for n in ["100", "500", "1000", "5000"]:
+    for n in ["100", "1000", "5000"]:
         # Load data for 'n' from specific contexts.
+        start_time1 = time.time()
         data1 = plot(test_file, country, "particular", n)
+        end_time1 = time.time()
+        start_time2 = time.time()
         data2 = plot(test_file, country, "general", n)
-
+        end_time2 = time.time()
+        execution_time1 = end_time1 - start_time1
+        execution_time2 = end_time2 - start_time2
+        dict_time[n] = [execution_time2, execution_time1]
         avg_1 = (sum(data1)) / (len(data1))
         avg_2 = (sum(data2)) / (len(data2))
         dict_result[n] = [avg_1, avg_2, avg_1 - avg_2]
-        print(dict_result[n])
-
+    
         # Create a logarithmic histogram for the data.
         bin_edges = np.logspace(0, 24, num=int(1E24 / 1E21) + 1)
         cdf1 = compute_cdf(data1, bin_edges)
@@ -65,7 +70,7 @@ def create_CDF(test_file, country):
         plt.ylim(0, 100)
         plt.legend(loc='lower right', fontsize='medium')
         plt.tight_layout(pad=2.0)
-        plt.title(f'Spain (Top-{n})')
-        plt.savefig(f'spain_{n}_trygskjjsl.png', dpi=300)
-
-    return dict_result
+        plt.title(f'{country}(Top-{n})')
+        plt.savefig(f'{country}_{n}.png', dpi=300)
+        
+    return dict_result, dict_time
